e503115b23a3878a2e82ba78c7331d7af6aca5ad
SergeevaMiaaa <92105666+SergeevaMiaaa@users.noreply.github.com>
2021-11-11 16:00:27 +0000

    9шгогш

---
+++ lec_1_data_type.py
@@ -0,0 +1,22 @@
+type
+
+type(3)
+
+print (type(3))
+
+print(type(3.4))
+
+print(type('Прелесть'))
+
+a = 'Prelesti'
+
+print (a)
+
+a = ['Prelest', 'Kruto', '6', 8]
+print(type(a))
+
+print(type(True))
+
+print(type(False))
+
+print(type(None))
---
+++ lec_1_obj_ref.py
@@ -0,0 +1,7 @@
+S = 3 + 4
+
+print (S)
+
+s = 5
+
+print (s)
